dec8ed0eeef02559567d85780c357941c3bcca0f
Poilon <poilon@gmail.com>
2020-05-26 12:09:06 +0200

    quizz

--- app/models/quizz.rb
+++ app/models/quizz.rb
@@ -5,4 +5,15 @@ class Quizz < ApplicationRecord
 
   belongs_to :user, optional: true
 
+  def self.generate_quizzs
+    q = Question.create(name: "Who won #{tournament.name} ?")
+    q.answers.create(name: 'Mang0')
+    a = q.answers.create(name: 'billibopeep')
+    q.answers.create(name: 'Poilon')
+    q.answers.create(name: 'Vek')
+    q.update(answer_id: a.id)
+    quizz = Quizz.create
+    quizz.questions << q
+  end
+
 end
