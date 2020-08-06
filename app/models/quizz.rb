d618f1e82cf62d6c0b3e538c2e6611de87edae9e
Yannis PERROT <vekoros@gmail.com>
2020-08-06 20:29:44 +0200

    frame data debut question

--- app/models/quizz.rb
+++ app/models/quizz.rb
@@ -47,6 +47,7 @@ class Quizz < ApplicationRecord
         tournaments = Tournament.joins(:results).group('tournaments.id').having('count(tournaments.id) > 100').order('RANDOM()').limit(1000)
         tournaments.each do |tournament|
           break if questions_count >= 5
+          next if tournament.results.count < 4
 
           questions_count += 1
           date = tournament.date.strftime("%d/%m/%Y")
@@ -73,16 +74,24 @@ class Quizz < ApplicationRecord
 
         countries_shuffled = countries.shuffle 
         countries_shuffled.each do |country|
-          break if questions_count >= 10
+          break if questions_count >= 9
           questions_count += 1
 
           states = { "AK" => "Alaska", "AL" => "Alabama", "AR" => "Arkansas", "AS" => "American Samoa", "AZ" => "Arizona", "CA" => "California", "CO" => "Colorado", "CT" => "Connecticut", "DC" => "District of Columbia", "DE" => "Delaware", "FL" => "Florida", "GA" => "Georgia", "GU" => "Guam", "HI" => "Hawaii", "IA" => "Iowa", "ID" => "Idaho", "IL" => "Illinois", "IN" => "Indiana", "KS" => "Kansas", "KY" => "Kentucky", "LA" => "Louisiana", "MA" => "Massachusetts", "MD" => "Maryland", "ME" => "Maine", "MI" => "Michigan", "MN" => "Minnesota", "MO" => "Missouri", "MS" => "Mississippi", "MT" => "Montana", "NC" => "North Carolina", "ND" => "North Dakota", "NE" => "Nebraska", "NH" => "New Hampshire", "NJ" => "New Jersey", "NM" => "New Mexico", "NV" => "Nevada", "NY" => "New York", "OH" => "Ohio", "OK" => "Oklahoma", "OR" => "Oregon", "PA" => "Pennsylvania", "PR" => "Puerto Rico", "RI" => "Rhode Island", "SC" => "South Carolina", "SD" => "South Dakota", "TN" => "Tennessee", "TX" => "Texas", "UT" => "Utah", "VA" => "Virginia", "VI" => "Virgin Islands", "VT" => "Vermont", "WA" => "Washington", "WI" => "Wisconsin", "WV" => "West Virginia", "WY" => "Wyoming" }
 
           if country == 'United States'
             usaStates = Player.where(country: 'United States').pluck(:state).uniq.compact.sort
-            random = rand(usaStates.length)
-            playerUSAstate = Player.where(state: usaStates[random]).order(elo: :desc).limit(4)
-            q = Question.create(name: "Who is the best player in the state of #{states[usaStates[random]]} in the USA?" )
+            usaStatesWithAtLeast4players = Array.new
+            usaStates.each do |c|
+              numberOfPlayersUSAstates = Player.where(country: 'United States', state: c).count()
+              if (numberOfPlayersUSAstates >= 4)
+                usaStatesWithAtLeast4players.push(c)
+              end
+            end
+            random = rand(usaStatesWithAtLeast4players.length)
+            playerUSAstate = Player.where(state: usaStatesWithAtLeast4players[random]).order(elo: :desc).limit(4)
+
+            q = Question.create(name: "Who is the best player in the state of #{states[usaStatesWithAtLeast4players[random]]} in the USA?" )
             a = q.answers.create(name: "#{playerUSAstate[0].name}")
             q.answers.create(name: "#{playerUSAstate[1].name}")
             q.answers.create(name: "#{playerUSAstate[2].name}")
@@ -100,6 +109,27 @@ class Quizz < ApplicationRecord
             quizz.questions << q
           end
         end
+
+        # frame data question
+        file = File.read(File.expand_path('../../../framedata/fox.json', __FILE__))
+        data_hash = JSON.parse(file)
+        moves = ["aerial", "smash", "tilt", "special", "jab", "dash", "grab", "dodge", "roll"]
+        randomMove = rand(moves.length)
+        move = moves[randomMove]
+        randomMoveNumber = rand(data_hash["attacks"][move].length)
+        #binding.pry
+        q = Question.create(name: "How much frame does the #{data_hash["attacks"][move][randomMoveNumber]["description"]} of #{data_hash["charname"]} last?")
+
+        frameTrue = data_hash["attacks"][move][randomMoveNumber]["total_frames"].to_i
+        frameFalse1 = data_hash["attacks"][move][randomMoveNumber]["total_frames"].to_i + 5
+        frameFalse2 = data_hash["attacks"][move][randomMoveNumber]["total_frames"].to_i - 5
+        frameFalse3 = data_hash["attacks"][move][randomMoveNumber]["total_frames"].to_i + 8
+        a = q.answers.create(name: "#{frameTrue}")
+        q.answers.create(name: "#{frameFalse1}")
+        q.answers.create(name: "#{frameFalse2}")
+        q.answers.create(name: "#{frameFalse3}")
+        q.update(answer_id: a.id)
+        quizz.questions << q
       end
     end
   end
